039ad9d0d5b2e4d09f54cf34c8a9604efabf34af
dim10000 <digitaloperator@gmail.com>
2016-03-21 22:02:00 +0000

    try2

--- ask/ask/urls.py
+++ ask/ask/urls.py
@@ -1,22 +1,9 @@
-from django.conf.urls import patterns, include, url
-
-from django.contrib import admin
-admin.autodiscover()
-
-from qa.views import main_list, test, question_details, popular_list, \
-ask_form, post_answer, signup_form, login_form
-
-urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'ask.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),i
-    url(r'^$', main_list, name='home'),
-    url(r'^login/', login_form),
-    url(r'^signup/', signup_form),
-    url(r'^question/(\d+)/', question_details, name='question-details'),
-    url(r'^ask/', ask_form),
-    url(r'^popular/', popular_list),
-    url(r'^new/', test),
-    url(r'^admin/', include(admin.site.urls)),
-    url(r'^answer/', post_answer),
-)
+urlpatterns = patterns('qa.views',
+   url(r'^$', 'test'),
+   url(r'^login/.*$', 'test', name='login'),
+   url(r'^signup/.*', 'test', name='signup'),
+   url(r'^question/(?P<id>[0-9]+)/$', 'test', name='question'),
+   url(r'^ask/.*', 'test', name='ask'),
+   url(r'^popular/.*', 'test', name='popular'),
+   url(r'^new/.*', 'test', name='new'),
+)  
